60b4e8c4d631b9fab232ed94365cf49b6fb927e2
JalapenosBud <Jalapenosbud@gmail.com>
2018-11-02 11:03:16 +0100

    initial commit

---
+++ CompositeAnimalCategorizer/src/com/company/Animal.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Animal extends AnimalComponent
+{
+    private String animalName;
+
+    public Animal(String animalName)
+    {
+        this.animalName = animalName;
+    }
+
+    @Override
+    public String getAnimalName()
+    {
+        return animalName;
+    }
+
+    public void displayAnimalInformation()
+    {
+        System.out.println("Animal name: " + getAnimalName());
+    }
+}
---
+++ CompositeAnimalCategorizer/src/com/company/Zoologist.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Zoologist
+{
+    AnimalComponent animalList;
+
+    public Zoologist(AnimalComponent animalList)
+    {
+        this.animalList = animalList;
+    }
+
+    public void getAnimalList()
+    {
+        animalList.displayAnimalInformation();
+    }
+
+}
